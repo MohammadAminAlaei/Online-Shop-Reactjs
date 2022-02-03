0ec1966a9780684bc4027b569e0b42934b13fc5f
MohammadAmin <mohammadaminalaee315@gmail.com>
2022-02-03 22:52:51 +0330

    Not Run Develope Branch

--- src/pages/Home/Home.page.js
+++ src/pages/Home/Home.page.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
 import {Helmet} from 'react-helmet';
-import {Typography} from '@mui/material';
-import {Header} from '../../components';
 
 class Home extends Component {
     render() {
@@ -10,8 +8,6 @@ class Home extends Component {
                 <Helmet>
                     <title>فروشگاه مکتب | خانه </title>
                 </Helmet>
-                <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque provident quod veniam. Debitis eaque expedita fuga in inventore itaque nihil obcaecati officia quae repudiandae. Accusamus dolore error porro suscipit vitae?</span><span>Alias at cum deserunt dicta ipsum iste numquam vitae! Error ipsam nemo omnis quam quis quod similique! Corporis et exercitationem harum neque optio reiciendis sit. Dicta molestiae nam nemo voluptate!</span><span>Adipisci at cum doloribus, est mollitia neque tempore. Et eum eveniet fuga nostrum! Aut cupiditate debitis dolorum fuga iusto, modi molestias mollitia non nostrum perspiciatis, rem sed sequi vitae, voluptatibus.</span><span>Autem distinctio porro sequi veritatis. Accusamus animi delectus eius iste non, omnis provident unde! Aspernatur consectetur debitis enim eos molestias natus quas quidem repudiandae? Ad excepturi facere illo possimus quia!</span><span>Ab esse ex ipsum, itaque obcaecati ut vitae. Accusamus aliquid, asperiores autem cum distinctio dolorem enim exercitationem incidunt laborum libero nemo non, provident quod quos recusandae, reiciendis sequi vero vitae!</span><span>Aperiam cupiditate dolores facilis ipsam odio quae reiciendis voluptates? Accusamus accusantium consectetur deserunt dolores eos, fugiat iste iure libero, magnam magni necessitatibus nostrum perferendis quisquam quos reprehenderit tempora velit. Vel.</span><span>Animi consectetur culpa eius eveniet illo impedit molestiae quasi sit vitae! Ad cupiditate, debitis delectus deleniti explicabo iusto, minima nulla odio optio praesentium sed similique unde veniam. Magni provident, unde?</span><span>Accusamus ad aliquid at dolor doloribus enim ex magni natus nemo nobis non numquam, porro quaerat quas repellat repellendus rerum totam unde vero voluptate. Fugit, iure quaerat? Exercitationem nulla, repellat?</span><span>Blanditiis cupiditate debitis distinctio, eos impedit iusto perferendis tempora totam vero vitae! Asperiores cum dolore error hic ipsam nisi provident quasi quo vitae voluptatibus? A adipisci consequuntur distinctio impedit incidunt.</span><span>Consequatur ipsam iusto nostrum obcaecati omnis vel? Alias cupiditate illo officia ratione voluptates. Ea explicabo minus molestiae nesciunt voluptatem! Amet consequatur corporis cumque et expedita fuga magnam quisquam saepe. Non.</span>
-                </p>
             </>
         );
     }
