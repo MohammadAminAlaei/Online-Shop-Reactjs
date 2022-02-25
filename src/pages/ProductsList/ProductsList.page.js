0c7b6153a77348bc11eb8c16c87775aa66a49f80
MohammadAmin <mohammadaminalaee315@gmail.com>
2022-02-25 08:33:34 +0330

    add fiture filter in page product-list and set paginations and fix many bugs

--- src/pages/ProductsList/ProductsList.page.js
+++ src/pages/ProductsList/ProductsList.page.js
@@ -3,47 +3,217 @@ import {Helmet} from 'react-helmet';
 import Box from '@mui/material/Box';
 import {Button, Skeleton, Typography} from '@mui/material';
 import {makeStyles} from '@mui/styles';
-import {useNavigate, useParams} from 'react-router-dom';
+import {useLocation, useNavigate, useParams} from 'react-router-dom';
 import http from 'services/http.service';
 import {PRODUCTS} from '../../configs/url.config';
 import Grid from '@mui/material/Grid';
-import {CardComponent} from '../../components';
+import {AppPagination, CardComponent} from 'components';
+import {ProSidebar, Menu, MenuItem, SubMenu, SidebarHeader, SidebarContent, SidebarFooter} from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
+import {Link} from 'react-router-dom';
+import {PATHS} from '../../configs/routes.config';
+import PropTypes from 'prop-types';
+import SelectUnstyled, {selectUnstyledClasses} from '@mui/base/SelectUnstyled';
+import OptionUnstyled, {optionUnstyledClasses} from '@mui/base/OptionUnstyled';
+import PopperUnstyled from '@mui/base/PopperUnstyled';
+import {styled} from '@mui/system';
+
+const blue = {
+    100: '#DAECFF',
+    200: '#99CCF3',
+    400: '#3399FF',
+    500: '#007FFF',
+    600: '#0072E5',
+    900: '#003A75',
+};
+
+const grey = {
+    100: '#E7EBF0',
+    200: '#E0E3E7',
+    300: '#CDD2D7',
+    400: '#B2BAC2',
+    500: '#A0AAB4',
+    600: '#6F7E8C',
+    700: '#3E5060',
+    800: '#2D3843',
+    900: '#1A2027',
+};
+
+const StyledButton = styled('button')(
+    ({theme}) => `
+  font-family: IBM Plex Sans, sans-serif;
+  font-size: 0.875rem;
+  box-sizing: border-box;
+  min-height: calc(1.5em + 22px);
+  min-width: 320px;
+  background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+  border: 1px solid ${theme.palette.mode === 'dark' ? grey[800] : grey[300]};
+  border-radius: 0.75em;
+  margin: 0.5em;
+  padding: 10px;
+  text-align: left;
+  line-height: 1.5;
+  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+
+  &:hover {
+    background: ${theme.palette.mode === 'dark' ? '' : grey[100]};
+    border-color: ${theme.palette.mode === 'dark' ? grey[700] : grey[400]};
+  }
+
+  &.${selectUnstyledClasses.focusVisible} {
+    outline: 3px solid ${theme.palette.mode === 'dark' ? blue[600] : blue[100]};
+  }
+
+  &.${selectUnstyledClasses.expanded} {
+    &::after {
+      content: '▴';
+    }
+  }
+
+  &::after {
+    content: '▾';
+    float: right;
+  }
+  `,
+);
+
+const StyledListbox = styled('ul')(
+    ({theme}) => `
+  font-family: IBM Plex Sans, sans-serif;
+  font-size: 0.875rem;
+  box-sizing: border-box;
+  padding: 5px;
+  margin: 10px 0;
+  min-width: 320px;
+  background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
+  border: 1px solid ${theme.palette.mode === 'dark' ? grey[800] : grey[300]};
+  border-radius: 0.75em;
+  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+  overflow: auto;
+  outline: 0px;
+  `,
+);
+
+const StyledOption = styled(OptionUnstyled)(
+    ({theme}) => `
+  list-style: none;
+  padding: 8px;
+  border-radius: 0.45em;
+  cursor: default;
+
+  &:last-of-type {
+    border-bottom: none;
+  }
+
+  &.${optionUnstyledClasses.selected} {
+    background-color: ${theme.palette.mode === 'dark' ? blue[900] : blue[100]};
+    color: ${theme.palette.mode === 'dark' ? blue[100] : blue[900]};
+  }
+
+  &.${optionUnstyledClasses.highlighted} {
+    background-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[100]};
+    color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+  }
+
+  &.${optionUnstyledClasses.highlighted}.${optionUnstyledClasses.selected} {
+    background-color: ${theme.palette.mode === 'dark' ? blue[900] : blue[100]};
+    color: ${theme.palette.mode === 'dark' ? blue[100] : blue[900]};
+  }
+
+  &.${optionUnstyledClasses.disabled} {
+    color: ${theme.palette.mode === 'dark' ? grey[700] : grey[400]};
+  }
+
+  &:hover:not(.${optionUnstyledClasses.disabled}) {
+    background-color: ${theme.palette.mode === 'dark' ? grey[800] : grey[100]};
+    color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
+  }
+  `,
+);
+
+const StyledPopper = styled(PopperUnstyled)`
+  z-index: 1;
+`;
+
+function CustomSelect(props) {
+    const components = {
+        Root: StyledButton,
+        Listbox: StyledListbox,
+        Popper: StyledPopper,
+        ...props.components,
+    };
+
+    return <SelectUnstyled {...props} components={components}/>;
+}
+
+CustomSelect.propTypes = {
+    /**
+     * The components used for each slot inside the Select.
+     * Either a string to use a HTML element or a component.
+     * @default {}
+     */
+    components: PropTypes.shape({
+        Listbox: PropTypes.elementType,
+        Popper: PropTypes.elementType,
+        Root: PropTypes.elementType,
+    }),
+};
 
 const useStyle = makeStyles(theme => ({
     sideBar: {
+        position: 'fixed',
+        right: 0,
         display: 'flex',
         flexDirection: 'column',
-        minHeight: '100vh',
         minWidth: '300px',
-        backgroundColor: '#fafafa',
+        minHeight: '100vh',
+        backgroundColor: '#c9daff42',
         borderLeft: '1px solid #e0e0e0',
-        justifyContent: 'right',
+        justifyContent: 'flex-start',
+        textAlign: 'right',
         [theme.breakpoints.down('sm')]: {
             maxHeight: 'max-content!important',
         }
     },
+    sidebarButton: {
+        lineHeight: '2rem!important',
+        fontSize: '1rem!important',
+    },
     orderBox: {
-        display: 'flex',
-        gap: '1rem',
+        marginTop: '-20px'
     }
 
 }));
-
 
 const ProductsList = () => {
 
     const {group} = useParams();
     const [data, setData] = useState([]);
     const navigate = useNavigate();
+    const [page, setPage] = useState(1);
+    const [numberOfPages, setNumberOfPages] = useState(10);
+    const [filter, setFilter] = useState('');
+    const [toggleMenu, setToggleMenu] = useState(true);
+    const [background, setBackground] = useState('گوشی موبایل');
+    const [value, setValue] = useState('');
+
+    const location = useLocation()
+    console.log(location);
 
     useEffect(() => {
-        http.get(`${PRODUCTS}?category.name=${group}`).then(res => {
-            setData(res.data)
+        http.get(`${PRODUCTS}?category.name=${group}${filter !== '' ? `&brand=${filter}` : ''}&_page=${page}&_limit=10`).then(res => {
+            setData(res.data);
+            setNumberOfPages(Math.ceil(res.data.length / 10));
         })
-    }, [group]);
+    }, [group, filter]);
 
     const handlePurchase = (e, id) => {
-        navigate(`purchase/${id}`);
+        navigate(`/purchase/${id}`);
+    }
+
+    const handleCategory = (category) => {
+        navigate(`/product-list/${category}`);
+        setBackground(category);
     }
 
     const skeletonNumber = [1, 2, 3, 4, 5, 6];
@@ -57,36 +227,70 @@ const ProductsList = () => {
             </Helmet>
             <Box className={classes.orderBox}>
                 <Box className={classes.sideBar}>
-                    <Button> کالاهای گروه گوشی مبایل </Button>
-                    <Button> کالاهای گروه لپتاپ </Button>
-                    <Button> کالاهای گروه کامپیوتر همه کاره </Button>
-                    <Button> کالاهای گروه هدفون </Button>
+                    <Button style={{background: background === 'گوشی موبایل' ? '#cdc7ff7a' : 'transparent'}}
+                            className={classes.sidebarButton}
+                            onClick={e => handleCategory('گوشی موبایل')}
+                            sx={{marginTop: '20px', justifyContent: 'flex-start'}}> کالاهای گروه گوشی موبایل </Button>
+                    <Button style={{background: background === 'لپتاپ' ? '#cdc7ff7a' : 'transparent'}}
+                            className={classes.sidebarButton} onClick={e => handleCategory('لپتاپ')}
+                            sx={{justifyContent: 'flex-start'}}> کالاهای
+                        گروه لپتاپ </Button>
+                    <Button style={{background: background === 'کامپیوتر همه کاره' ? '#cdc7ff7a' : 'transparent'}}
+                            className={classes.sidebarButton} onClick={e => handleCategory('کامپیوتر همه کاره')}
+                            sx={{justifyContent: 'flex-start'}}> کالاهای
+                        گروه کامپیوتر همه کاره </Button>
+                    <Button style={{background: background === 'هدفون بی سیم' ? '#cdc7ff7a' : 'transparent'}}
+                            className={classes.sidebarButton} onClick={e => handleCategory('هدفون بی سیم')}
+                            sx={{justifyContent: 'flex-start'}}> کالاهای
+                        گروه هدفون </Button>
+                    <Typography sx={{fontSize: '.9rem', marginTop: '8px', padding: '0 .6rem'}}> فیلتر بر
+                        اساس: </Typography>
+                    <CustomSelect value={filter} onChange={setFilter}>
+                        <StyledOption value="">هیچ چیز</StyledOption>
+                        <StyledOption value="اپل">اپل</StyledOption>
+                        <StyledOption value="شیائومی">شیائومی</StyledOption>
+                        <StyledOption value="مودیو">مودیو</StyledOption>
+                        <StyledOption value="سامسونگ">سامسونگ</StyledOption>
+                        <StyledOption value="هایلو">هایلو</StyledOption>
+                        <StyledOption value="ام اس آی ">ام اس آی</StyledOption>
+                        <StyledOption value="اچ پی">اچ پی</StyledOption>
+                        <StyledOption value="یونیو">یونیو</StyledOption>
+                        <StyledOption value="لنوو">لنوو</StyledOption>
+                        <StyledOption value="کرون">کرون</StyledOption>
+                    </CustomSelect>
                 </Box>
-                <Box>
-                    <Typography variant="h5" sx={{my: 2}}> کالاهای گروه {group}</Typography>
-                    <Grid container spacing={{xs: 2, md: 6}}
-                          columns={{xs: 12, sm: 12, md: 12}}>
+                <Box sx={{paddingTop: '20px'}}>
+                    <Typography variant="h5" sx={{my: 3}}> کالاهای گروه {group}</Typography>
+                    <Grid container spacing={{xs: 2, md: 4}}
+                          columns={{xs: 12, sm: 8, md: 12}}>
                         {!!data.length ? data.map(item => (
-                            <Grid item xs={12} sm={12} md={6} key={item.id}>
-                                <CardComponent
-                                    img={item.image[0]}
-                                    price={item.price} firstName={item.firstName} brand={item.brand}
-                                    description={item.description}
-                                    onClick={e => handlePurchase(e, item.id)}
-                                />
-                            </Grid>
-                        )) : (skeletonNumber.map((item, index) => (
-                                <Grid item xs={12} sm={4} md={4} key={index}>
-                                    <Skeleton key={item} animation="wave" variant="rect" width={'300px'}
-                                              height={'250px'}/>
+                                <Grid item xs={12} sm={4} md={4} key={item.id}>
+                                    <CardComponent
+                                        img={item.image[0]}
+                                        price={item.price} firstName={item.firstName} brand={item.brand}
+                                        description={item.description}
+                                        onClick={e => handlePurchase(e, item.id)}
+                                    />
                                 </Grid>
-                            ))
-                        )}
+                            )) :
+                            //     (skeletonNumber.map((item, index) => (
+                            //         <Grid item xs={12} sm={4} md={4} key={index}>
+                            //             <Skeleton key={item} animation="wave" variant="rect" width={'300px'}
+                            //                       height={'250px'}/>
+                            //         </Grid>
+                            //     ))
+                            // )
+                            (
+                                <Typography sx={{padding: '1.7rem', marginTop: '20px', fontFamily: 'Vazir-bold'}}
+                                            variant="h5"> کالاهای مورد
+                                    نظر ناموجود
+                                    است </Typography>
+                            )
+                        }
                     </Grid>
                 </Box>
             </Box>
-
-
+            <AppPagination setPage={setPage} pageNumber={numberOfPages}/>
         </>
     );
 }
